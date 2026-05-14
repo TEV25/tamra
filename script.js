262dee52fa43ef463d5e02affad954d0c3a78d0a
TEV25 <naif93382@gmail.com>
2026-05-14 23:15:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -532,8 +532,3 @@ window.onclick = function(e) {
 };
 
 updateCartDisplay();
-// إخفاء السلة والنافذة الشفافة في البداية
-const sidebar = document.getElementById("cartSidebar");
-const overlay = document.getElementById("cartOverlay");
-if (sidebar) sidebar.classList.remove("open");
-if (overlay) overlay.classList.remove("show");
